3bfdbf39d03b30912c109e476996b67c90a490af
Ikuchar1 <ian1kuchar@gmail.com>
2024-10-22 23:57:39 -0500

    Update unit_test.py
    added more unit tests

--- unit_test.py
+++ unit_test.py
@@ -27,7 +27,8 @@ class TestPathFinding(unittest.TestCase):
         self.assertNotEqual(almost_pi, pi)
         self.assertAlmostEqual(first=almost_pi, second=pi, delta=1e-1)
         
-    def test_dfs_happyCase(self):
+    #random pathing test
+    def test_random_hits_required_nodes(self):
         
         graph_index = 0
         target_node = 4
@@ -37,28 +38,72 @@ class TestPathFinding(unittest.TestCase):
         
         graph_data.graph_data = {
             graph_index: [
-                (0, [1, 2]),
-                (1, [3]),
-                (2, [3, 4]),
-                (3, [5]),
-                (4, [5]),
-                (5, [6, 7]),
-                (6, [8]),
-                (7, [8]),
-                (8, [9]),
-                (9, [])
+                [(0, 0), [1, 4]],
+                [(0, 100), [0, 2, 5]],
+                [(0, 200), [1, 3, 6]],
+                [(0, 300), [2, 7]],
+                [(100, 0), [5, 0, 8]],
+                [(100, 100), [4, 6, 1, 9]],
+                [(100, 200), [5, 7, 2, 10]],
+                [(100, 300), [6, 3, 11]],
+                [(200, 0), [9, 4, 12]],
+                [(200, 100), [8, 10, 5, 13]],
+                [(200, 200), [9, 11, 6, 14]],
+                [(200, 300), [10, 7, 15]],
+                [(300, 0), [13, 8]],
+                [(300, 100), [12, 14, 9]],
+                [(300, 200), [13, 15, 10]],
+                [(300, 300), [14,11]]
+            ]
+        }
+        
+        #path it actually follows
+        path = get_random_path()
+        
+        #check to make sure it starts at start, hits target, and ends at exit node
+        self.assertEqual(path[0], 0, "The path does not start at the start node")
+        self.assertIn(target_node, path, "The path does not include the target node")
+        self.assertEqual(path[-1], 15, "The path does not end at the exit node")
+    
+    #tests to make sure it gets correct path
+    def test_dfs_happy_case(self):
+        
+        graph_index = 0
+        target_node = 4
+        
+        global_game_data.current_graph_index = graph_index
+        global_game_data.target_node = {graph_index: target_node}
+        
+        graph_data.graph_data = {
+            graph_index: [
+                [(0, 0), [1, 4]],
+                [(0, 100), [0, 2, 5]],
+                [(0, 200), [1, 3, 6]],
+                [(0, 300), [2, 7]],
+                [(100, 0), [5, 0, 8]],
+                [(100, 100), [4, 6, 1, 9]],
+                [(100, 200), [5, 7, 2, 10]],
+                [(100, 300), [6, 3, 11]],
+                [(200, 0), [9, 4, 12]],
+                [(200, 100), [8, 10, 5, 13]],
+                [(200, 200), [9, 11, 6, 14]],
+                [(200, 300), [10, 7, 15]],
+                [(300, 0), [13, 8]],
+                [(300, 100), [12, 14, 9]],
+                [(300, 200), [13, 15, 10]],
+                [(300, 300), [14,11]]
             ]
         }
         
         #path it should follow
-        path = [0, 2, 4, 5, 7, 8, 9]
+        path = [0, 4, 8, 12, 13, 9, 5, 1, 2, 6, 10, 14, 15]
+        
         #path it actually follows
         real_path = get_dfs_path()
                 
         self.assertEqual(path, real_path)
         
-        
-    def test_bfs_happyCase(self):
+    def test_dfs_hits_required_nodes(self):
         
         graph_index = 0
         target_node = 4
@@ -68,25 +113,106 @@ class TestPathFinding(unittest.TestCase):
         
         graph_data.graph_data = {
             graph_index: [
-                (0, [1, 2]),
-                (1, [3]),
-                (2, [3, 4]),
-                (3, [5]),
-                (4, [5]),
-                (5, [6, 7]),
-                (6, [8]),
-                (7, [8]),
-                (8, [9]),
-                (9, [])
+                [(0, 0), [1, 4]],
+                [(0, 100), [0, 2, 5]],
+                [(0, 200), [1, 3, 6]],
+                [(0, 300), [2, 7]],
+                [(100, 0), [5, 0, 8]],
+                [(100, 100), [4, 6, 1, 9]],
+                [(100, 200), [5, 7, 2, 10]],
+                [(100, 300), [6, 3, 11]],
+                [(200, 0), [9, 4, 12]],
+                [(200, 100), [8, 10, 5, 13]],
+                [(200, 200), [9, 11, 6, 14]],
+                [(200, 300), [10, 7, 15]],
+                [(300, 0), [13, 8]],
+                [(300, 100), [12, 14, 9]],
+                [(300, 200), [13, 15, 10]],
+                [(300, 300), [14,11]]
+            ]
+        }
+        
+        #path it actually follows
+        path = get_dfs_path()
+        
+        #check to make sure it starts at start, hits target, and ends at exit node
+        self.assertEqual(path[0], 0, "The path does not start at the start node")
+        self.assertIn(target_node, path, "The path does not include the target node")
+        self.assertEqual(path[-1], 15, "The path does not end at the exit node")
+     
+    #tests to make sure it gets correct path
+    def test_bfs_happy_case(self):
+        
+        graph_index = 0
+        target_node = 4
+        
+        global_game_data.current_graph_index = graph_index
+        global_game_data.target_node = {graph_index: target_node}
+        
+        graph_data.graph_data = {
+            graph_index: [
+                [(0, 0), [1, 4]],
+                [(0, 100), [0, 2, 5]],
+                [(0, 200), [1, 3, 6]],
+                [(0, 300), [2, 7]],
+                [(100, 0), [5, 0, 8]],
+                [(100, 100), [4, 6, 1, 9]],
+                [(100, 200), [5, 7, 2, 10]],
+                [(100, 300), [6, 3, 11]],
+                [(200, 0), [9, 4, 12]],
+                [(200, 100), [8, 10, 5, 13]],
+                [(200, 200), [9, 11, 6, 14]],
+                [(200, 300), [10, 7, 15]],
+                [(300, 0), [13, 8]],
+                [(300, 100), [12, 14, 9]],
+                [(300, 200), [13, 15, 10]],
+                [(300, 300), [14,11]]
             ]
         }
         
         #path it should follow
-        path = [0, 2, 4, 5, 6, 8, 9]
+        path = [0, 4, 5, 6, 7, 11, 15]
         #path it actually follows
         real_path = get_bfs_path()
         
         self.assertEqual(path, real_path)
+        
+    def test_bfs_hits_required_nodes(self):
+        
+        graph_index = 0
+        target_node = 4
+        
+        global_game_data.current_graph_index = graph_index
+        global_game_data.target_node = {graph_index: target_node}
+        
+        graph_data.graph_data = {
+            graph_index: [
+                [(0, 0), [1, 4]],
+                [(0, 100), [0, 2, 5]],
+                [(0, 200), [1, 3, 6]],
+                [(0, 300), [2, 7]],
+                [(100, 0), [5, 0, 8]],
+                [(100, 100), [4, 6, 1, 9]],
+                [(100, 200), [5, 7, 2, 10]],
+                [(100, 300), [6, 3, 11]],
+                [(200, 0), [9, 4, 12]],
+                [(200, 100), [8, 10, 5, 13]],
+                [(200, 200), [9, 11, 6, 14]],
+                [(200, 300), [10, 7, 15]],
+                [(300, 0), [13, 8]],
+                [(300, 100), [12, 14, 9]],
+                [(300, 200), [13, 15, 10]],
+                [(300, 300), [14,11]]
+            ]
+        }
+        
+        #path it actually follows
+        path = get_bfs_path()
+        
+        #check to make sure it starts at start, hits target, and ends at exit node
+        self.assertEqual(path[0], 0, "The path does not start at the start node")
+        self.assertIn(target_node, path, "The path does not include the target node")
+        self.assertEqual(path[-1], 15, "The path does not end at the exit node")
 
 if __name__ == '__main__':
     unittest.main()
